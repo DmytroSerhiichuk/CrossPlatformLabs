b90e63f651da3b5c90b78d4c8197e9a38bc4b88b
DmytroSerhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-09-18 21:37:13 +0300

    Lab 1 - input reading

---
+++ Lab_1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab_1;
+
+class Program
+{
+    private const string ValidChars = "0123456789abcdef?";
+
+    static void Main(string[] args)
+    {
+        (var p1, var p2) = ReadInput();
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+    }
+
+    static (string, string) ReadInput()
+    {
+        using (var sr = new StreamReader("./INPUT.txt"))
+        {
+            var p1 = sr.ReadLine().Trim();
+            var p2 = sr.ReadLine().Trim();
+
+            CheckInputs(p1, p2);
+
+            return (p1, p2);
+        }
+    }
+
+    static void CheckInputs(string p1, string p2)
+    {
+        if (p1.Length != p2.Length)
+        {
+            throw new ArgumentException("Вхідні дані мають бути однакової довжини");
+        }
+
+        if (p1.Length == 0 || p1.Length > 9 || p2.Length == 0 || p2.Length > 9)
+        {
+            throw new ArgumentException("Вхідні дані мають бути довжиною від 1 до 9");
+        }
+
+        if (p1.Any(c => !ValidChars.Contains(c)) || p2.Any(c => !ValidChars.Contains(c)))
+        {
+            throw new ArgumentException($"Допустимі символи: {string.Join(", ", ValidChars.ToCharArray())}");
+        }
+    }
+}
